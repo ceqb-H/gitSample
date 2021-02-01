a42f60d7306f504b5b74968142013a86ee33f3cf
WF.Hsiao <3edc8787@gmail.com>
2021-02-01 16:24:41 +0800

    3nd Version.

--- gitTest/src/gitTest1/mainapp.java
+++ gitTest/src/gitTest1/mainapp.java
@@ -3,5 +3,9 @@ package gitTest1;
 public class mainapp {
 	public static void main(String[] args) {
 		System.out.println("2nd Version.");
+		
+		//user2 modify
+		System.out.println("3nd Version.");
+		
 	}
 }
